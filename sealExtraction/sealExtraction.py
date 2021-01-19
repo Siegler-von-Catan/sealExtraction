5ed739e1d4b409b9d1f7298723c42f2702900958
Joana Be <33000454+JoeAtHPI@users.noreply.github.com>
2021-01-19 01:15:16 +0100

    Renames main program and adds example

--- sealExtraction/sealExtraction.py
+++ sealExtraction/sealExtraction.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 #     SealExtraction - Extracting the motive out of stamps with Image Processing.
 #     Copyright (C) 2021
 #     Joana Bergsiek, Leonard Geier, Lisa Ihde, Tobias Markus, Dominik Meier, Paul Methfessel
@@ -15,15 +17,61 @@
 #     You should have received a copy of the GNU General Public License
 #     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+'''
+Detecting seals' motives for Paul Arnold Grun's Data set (https://codingdavinci.de/de/daten/siegelsammlung-paul-arnold-grun).
+
+We do not care about the writing, background or the edges of the pressed down wax. Thus the extracted result will only contain the shape and motive
+of the used stamp.
+
+Usage:
+  sealExtraction.py [-h] --o OUTPUT INPUT
+
+Keys:
+  ESC   - exit
+'''
+
+from glob import glob
+
+import argparse
+import sys
+import os
+
+import cv2 as cv
+
 from segmentMotive import segmentMotive
 from segmentWax import segmentWax
 
-def segmentSeal(image):
+inputPath = None
+
+def initializArgumentParser():
     """
-    First, segment the wax from the image which contains the motive.
-    Then, segment out the motive in the wax.
+    Initialize documentation for the command line arguments and return the arguments extracted
     """
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-o", "--output", required=True, help='output file')
+    ap.add_argument('input', metavar="INPUT", help='input file')
+    return ap
+
+
+def main():
+    """
+    Read in jpg image,
+    Extract the white background,
+    Extract Motive,
+    Save each result as folder in given path
+    """
+
+    ap = initializArgumentParser()
+    args = vars(ap.parse_args())
+
+    image = cv.imread(args['input'])
     segmentedWax = segmentWax(image)
     segmentedMotive = segmentMotive(segmentedWax)
-    return segmentedMotive
+    result =  segmentedMotive
+    cv.imwrite(args['output'], result)
+
+    cv.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
 
