0904d2d47de99cd60d7b1e03d830ade61fafaa7b
XPerianer <dominik.meier@student.hpi.de>
2021-08-26 18:13:47 +0200

    Use whole image as mask if else fails (backup plan)

--- sealExtraction/segmentMotive.py
+++ sealExtraction/segmentMotive.py
@@ -26,16 +26,16 @@ from sealExtraction.scoring import getCriteriaWeightedScores, getAngleDifference
 from sealExtraction.utils import getRelativeMaskSizeToWaxSize, drawGivenContoursOn, cropImageToContourAABB
 
 tenChooseTwoCache = [
-    [1, 0],[2, 0],[2, 1],[3, 0],[3, 1],
-    [3, 2],[4, 0],[4, 1],[4, 2],[4, 3],
-    [5, 0],[5, 1],[5, 2],[5, 3],[5, 4],
-    [6, 0],[6, 1],[6, 2],[6, 3],[6, 4],
-    [6, 5],[7, 0],[7, 1],[7, 2],[7, 3],
-    [7, 4],[7, 5],[7, 6],[8, 0],[8, 1],
-    [8, 2],[8, 3],[8, 4],[8, 5],[8, 6],
-    [8, 7],[9, 0],[9, 1],[9, 2],[9, 3],
-    [9, 4],[9, 5],[9, 6],[9, 7],[9, 8],
-    ]
+    [1, 0], [2, 0], [2, 1], [3, 0], [3, 1],
+    [3, 2], [4, 0], [4, 1], [4, 2], [4, 3],
+    [5, 0], [5, 1], [5, 2], [5, 3], [5, 4],
+    [6, 0], [6, 1], [6, 2], [6, 3], [6, 4],
+    [6, 5], [7, 0], [7, 1], [7, 2], [7, 3],
+    [7, 4], [7, 5], [7, 6], [8, 0], [8, 1],
+    [8, 2], [8, 3], [8, 4], [8, 5], [8, 6],
+    [8, 7], [9, 0], [9, 1], [9, 2], [9, 3],
+    [9, 4], [9, 5], [9, 6], [9, 7], [9, 8],
+]
 
 
 def segmentMotive(waxSegmentedImage):
@@ -44,7 +44,7 @@ def segmentMotive(waxSegmentedImage):
     threshold image. With that mask we can pair contours, fit an ellipse through them and choose the one with the highest probabilty
     of containing the motive. That fitted elipse is then choosen as the motive.
     """
-    waxThreshold = cv.threshold( cv.cvtColor(waxSegmentedImage, cv.COLOR_BGR2GRAY), 1, 255, cv.THRESH_BINARY)[1]
+    waxThreshold = cv.threshold(cv.cvtColor(waxSegmentedImage, cv.COLOR_BGR2GRAY), 1, 255, cv.THRESH_BINARY)[1]
     saliencyMask = getSaliencyMask(waxSegmentedImage)
     referenceImage = getReferenceImageFor(waxSegmentedImage)
 
@@ -62,21 +62,22 @@ def segmentMotive(waxSegmentedImage):
     contours = sorted(contours, key=cv.contourArea, reverse=True)[:10]
     # todo end
 
-    waxThreshold = cv.threshold( cv.cvtColor(waxSegmentedImage, cv.COLOR_BGR2GRAY), 1, 255, cv.THRESH_BINARY)[1]
+    waxThreshold = cv.threshold(cv.cvtColor(waxSegmentedImage, cv.COLOR_BGR2GRAY), 1, 255, cv.THRESH_BINARY)[1]
     numberOfWaxPixels = np.where(waxThreshold > 0)[0]
     mask = getMostLikelyMaskFor(contours, tenChooseTwoCache, referenceImage, len(numberOfWaxPixels), waxSegmentedImage)
     maskContours = cv.findContours(mask, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
     maskContours = imutils.grab_contours(maskContours)
 
-    contourMask = np.zeros(waxSegmentedImage.shape[:2],np.uint8)
+    contourMask = np.zeros(waxSegmentedImage.shape[:2], np.uint8)
     cv.drawContours(contourMask, maskContours, -1, (255), -1)
 
     waxSegmentedImage = cv.cvtColor(waxSegmentedImage, cv.COLOR_BGR2BGRA)
-    waxSegmentedImage = cv.bitwise_and(waxSegmentedImage,waxSegmentedImage,mask = contourMask)
+    waxSegmentedImage = cv.bitwise_and(waxSegmentedImage, waxSegmentedImage, mask=contourMask)
     waxSegmentedImage = cropImageToContourAABB(waxSegmentedImage, maskContours[0])
 
     print("Done segmenting image")
     return waxSegmentedImage
+
 
 def cropImageToContoursAABB(image, contours):
     points = np.concatenate(
@@ -84,10 +85,12 @@ def cropImageToContoursAABB(image, contours):
          contours[5], contours[6], contours[7], contours[8], contours[9]), axis=0)
     return cropImageToContourAABB(image, points)
 
+
 def getReferenceImageFor(waxSegmentedImage):
     waxSegmentedImage = cv.GaussianBlur(waxSegmentedImage, (21, 21), 0)
     sobel = getSobelOf(waxSegmentedImage)
     return sobel
+
 
 def getSobelOf(image):
     gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
@@ -100,30 +103,40 @@ def getSobelOf(image):
     gradient = cv.subtract(gradX, gradY)
     gradient = cv.convertScaleAbs(gradient)
 
-    thresh = cv.threshold(gradient,0,255,cv.THRESH_BINARY | cv.THRESH_OTSU)[1]
+    thresh = cv.threshold(gradient, 0, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)[1]
     return thresh
 
+
 def getMostLikelyMaskFor(allContours, contoursPairs, referenceImage, numberOfWaxPixels, original):
-    pairMasks = [getMasksForContourPair(allContours[pair[0]], allContours[pair[1]], referenceImage, numberOfWaxPixels) for pair in contoursPairs]
+    pairMasks = [getMasksForContourPair(allContours[pair[0]], allContours[pair[1]], referenceImage, numberOfWaxPixels)
+                 for pair in contoursPairs]
     pairMasks = list(filter(lambda masks: len(masks) > 0, pairMasks))
 
     if all(len(element) == 0 for element in pairMasks):
         print("Filtered out all masks, reiterating with disabled filter")
-        pairMasks = [getMasksForContourPair(allContours[pair[0]], allContours[pair[1]], referenceImage, numberOfWaxPixels, enable_filter=False) for pair in contoursPairs]
+        pairMasks = [
+            getMasksForContourPair(allContours[pair[0]], allContours[pair[1]], referenceImage, numberOfWaxPixels,
+                                   enable_filter=False) for pair in contoursPairs]
         pairMasks = list(filter(lambda masks: len(masks) > 0, pairMasks))
-
+        if not pairMasks:
+            print("This image can not be cropped properly, using whole image as mask")
+            boxMask = np.full(referenceImage.shape[:2], 255, np.uint8)
+            return boxMask
 
     scoreSums = getCriteriaWeightedScores(pairMasks, referenceImage, numberOfWaxPixels)
 
     return getMaskWithHighestScore(scoreSums, pairMasks)
 
+
 def drawPairMasksAndScoresOn(pairMasks, scoreSums, imageToDrawOn):
     for (index, shapeMasks) in enumerate(pairMasks):
         drawMasksAndScoresOn(shapeMasks, scoreSums[index], imageToDrawOn)
 
+
 def drawMasksAndScoresOn(shapeMasks, scoreSums, imageToDrawOn):
     for (index, shapeMask) in enumerate(shapeMasks):
         drawMaskAndSumOnImage(shapeMask, scoreSums[index], imageToDrawOn)
+
 
 def drawMaskAndSumOnImage(shapeMask, scoreSum, imageToDrawOn):
     shapeContours = cv.findContours(shapeMask, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
@@ -131,9 +144,10 @@ def drawMaskAndSumOnImage(shapeMask, scoreSum, imageToDrawOn):
     ensure(len(shapeContours) == 1).equals(True)
 
     for (i, c) in enumerate(shapeContours):
-        x,y,w,h = cv.boundingRect(c)
+        x, y, w, h = cv.boundingRect(c)
         cv.putText(imageToDrawOn, "{}".format(scoreSum), (int(x), int(y)), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
         cv.drawContours(imageToDrawOn, [c], -1, (0, 255, 0), 6)
+
 
 def getMaskWithHighestScore(scoreSums, pairMasks):
     highestValueInPairValues = list(map(lambda values: max(values), scoreSums))
@@ -144,20 +158,26 @@ def getMaskWithHighestScore(scoreSums, pairMasks):
     belongingMask = maxValues.index(max(maxValues))
     return belongingMasks[belongingMask]
 
+
 def getMasksForContourPair(contourA, contourB, referenceImage, numberOfWaxPixels, enable_filter=True):
     """
     Create basic shapes to test how likely they contain the motive
     """
-    masks = getShapeMasksForContourPair(contourA, contourB, referenceImage)
+    try:
+        masks = getShapeMasksForContourPair(contourA, contourB, referenceImage)
+    except:
+        return []
 
     if enable_filter:
         return list(filter(lambda mask: not shouldBeFilteredOut(mask, referenceImage, numberOfWaxPixels), masks))
     return list(filter(lambda mask: not mustBeFilteredOut(mask), masks))
 
+
 def mustBeFilteredOut(mask):
     if isEmptyMask(mask):
         return True
     return False
+
 
 def shouldBeFilteredOut(mask, referenceImage, numberOfWaxPixels):
     if mustBeFilteredOut(mask):
@@ -176,17 +196,20 @@ def shouldBeFilteredOut(mask, referenceImage, numberOfWaxPixels):
         return True
     return False
 
+
 def horizontalRelationToHeight(shapeMask):
     shapeContours = cv.findContours(shapeMask, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
     shapeContours = imutils.grab_contours(shapeContours)
     ensure(len(shapeContours) == 1).equals(True)
 
-    x,y,w,h = cv.boundingRect(shapeContours[0])
+    x, y, w, h = cv.boundingRect(shapeContours[0])
 
-    return w/h
+    return w / h
+
 
 def isEmptyMask(mask):
     return not np.any(mask > 0)
+
 
 def boundingBoxIsTouchingImageBorder(shapeMask, referenceImage):
     shapeContours = cv.findContours(shapeMask, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
@@ -194,29 +217,30 @@ def boundingBoxIsTouchingImageBorder(shapeMask, referenceImage):
     ensure(len(shapeContours) == 1).equals(True)
 
     imageHeight, imageWidth = referenceImage.shape
-    x,y,w,h = cv.boundingRect(shapeContours[0])
-    if x == 0 or y == 0 or x+w >= imageWidth-1 or y+h >= imageHeight-1: return True
+    x, y, w, h = cv.boundingRect(shapeContours[0])
+    if x == 0 or y == 0 or x + w >= imageWidth - 1 or y + h >= imageHeight - 1: return True
 
     return False
+
 
 def getShapeMasksForContourPair(contourA, contourB, referenceImage):
     points = np.concatenate((contourA, contourB))
     ellipse = cv.fitEllipse(points)
 
-    (x,y),radius = cv.minEnclosingCircle(points)
-    center = (int(x),int(y))
+    (x, y), radius = cv.minEnclosingCircle(points)
+    center = (int(x), int(y))
     radius = int(radius)
 
     rect = cv.minAreaRect(points)
     box = cv.boxPoints(rect)
     box = np.int0(box)
 
-    ellipseMask = np.zeros(referenceImage.shape[:2],np.uint8)
-    circleMask = np.zeros(referenceImage.shape[:2],np.uint8)
-    boxMask = np.zeros(referenceImage.shape[:2],np.uint8)
+    ellipseMask = np.zeros(referenceImage.shape[:2], np.uint8)
+    circleMask = np.zeros(referenceImage.shape[:2], np.uint8)
+    boxMask = np.zeros(referenceImage.shape[:2], np.uint8)
 
-    cv.ellipse(ellipseMask,ellipse,255,-1)
-    cv.circle(circleMask,center,radius,255,-1)
-    cv.drawContours(boxMask,[box],0,255,-1)
+    cv.ellipse(ellipseMask, ellipse, 255, -1)
+    cv.circle(circleMask, center, radius, 255, -1)
+    cv.drawContours(boxMask, [box], 0, 255, -1)
 
     return [ellipseMask, circleMask, boxMask]
